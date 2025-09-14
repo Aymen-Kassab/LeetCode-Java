96dc4c365151ebb7acda6fd78b3377c42e8e6b18
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-15 00:09:40 +0100

    Done with TwoSum and palindrome number problem

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,5 +1,23 @@
 public class PalindromeNumber {
     public static void main(String[] arg){
+        int x = 1232;
+        System.out.println(isPalindrome(x));
+    }
 
+    public static boolean isPalindrome(int x){
+        String str = String.valueOf(x);
+        int i = str.length() - 1; // The last index
+        int j = 0;
+
+        if (x < 0) return false; // Negative numbers are not palindrome
+
+        while(i > j){
+            if (str.charAt(j) != str.charAt(i)){
+                return false;
+            }
+            i--;
+            j++;
+        }
+        return true;
     }
 }
