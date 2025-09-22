5ec0b8a55d04d8655185a9a4163c323afe7c4730
Aymen-Kassab <aymankassab123@gmail.com>
2025-09-22 21:03:28 +0100

    I solved Longest Common Prefix problem

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LongestCommonPrefix {
     public static void main(String[] args){
-        String [] array = {"flower","fow","fight"};
-        System.out.println("The prefix is: " + longestCommonPrefix(array));
+        String [] array = {"flower", "flower", "flower"};
+        System.out.println(longestCommonPrefix(array));
     }
 
     public static String longestCommonPrefix(String[] strs){
@@ -14,20 +16,27 @@ public class LongestCommonPrefix {
         int shortestString = 0;
         String result = "";
 
-        //Finding the shortest string in the array
-        for(int i = 0; i < length; i++){
-            if(i < length - 1 && strs[i].length() > strs[i+1].length()){
-                shortestString = strs[i + 1].length();
-            }
+        //Basic checking
+        if(length == 1){
+            return strs[0];
+        }
+        else if(length == 0){
+            return "";
         }
 
+        //Finding the shortest string in the array
+        Arrays.sort(strs, Comparator.comparingInt(String::length));
+        shortestString = strs[0].length();
+        System.out.println(shortestString);
+
+        //Getting the prefix
         for(int i = 0; i < shortestString; i++){
             for(int j = 0; j < length; j++){
                 if (j < length -1 && strs[j].charAt(i) != strs[j+1].charAt(i)){ // The problem is here
                     return result;
                 }
             }
-            prefixList.add(strs[i].charAt(i));
+            prefixList.add(strs[0].charAt(i));
             result = prefixList.stream()
                     .map(String::valueOf)
                     .collect(Collectors.joining());
